4a6f7343e60de0a5d0589a48694061b54d3222a0
AshishSharma <82713715+aShisH-SharMa28@users.noreply.github.com>
2021-06-17 01:26:00 +0530

    Update day0_exercise.py

--- day0_exercise.py
+++ day0_exercise.py
@@ -1,1 +1,3 @@
-print("Hey guys...this is the code so just copy-paste and run it and it'll give you my introduction ig!!\nSoo...Ashish Sharma this side, I'm from Sawai Madhopur, Rajasthan\n\nCurrently I am a B-tech CSE student of UPES\nthree things I love to do are :-\n(1). Travelling,\n(2). sports\n(3). and anime\n\nThree things I don't like are :-\n(1). Pull ups\n(2).")
+print("Hey guys...this is the code so just copy-paste and run it and it'll give you my introduction ig!!\nSoo...Ashish Sharma this side, I'm from Sawai Madhopur, Rajasthan\n\nCurrently I am a B-tech CSE student of UPES\nthree things I love to do are :-\n(1). Travelling,\n(2). sports\n(3). and anime\n\nThree things I don't like are :-\n(1). Pull ups\n(2). Stupid long talks\n(3). Errors :)")
+
+print("\n\t~~~~\t~~~~\n\t @  \t @\n\t     #\n\t\t     i know this is stupid..!!\n\t   ----\n")
