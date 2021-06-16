d562c8ff2c156eac69cdbc2e1e243dcfae912e61
Ashish Sharma <28ashish.sharma28@gmail.com>
2021-06-16 22:37:37 +0530

    first commit

---
+++ day0_exercise.py
@@ -0,0 +1,1 @@
+print("Hey guys...this is the code so just copy-paste and run it and it'll give you my introduction ig!!\nSoo...Ashish Sharma this side, I'm from Sawai Madhopur, Rajasthan\n\nCurrently I am a B-tech CSE student of UPES\nthree things I love to do are :-\n(1). Travelling,\n(2). sports\n(3). and anime\n\nThree things I don't like are :-\n(1). Pull ups\n(2).")
